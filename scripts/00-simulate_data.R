a890762867c0980778215567914974482f7b0973
jackchinski <marcin.jacz@gmail.com>
2023-04-20 16:41:35 -0400

    data results, discussion and limitations

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,10 +10,50 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(purrr)
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+
+set.seed(123)
+
+#Create list of possible answers in random data (excluding nonresponse answers since they will be omitted in real data)
+minority = c("yes", "no")
+age = c(29, 73)
+gender = c("male", "female")
+credits = c("single", "more")
+beauty = c(-2.0, 2.0)
+eval = c(0, 5.0)
+division = c("upper", "lower")
+native = c("yes", "no")
+tenure = c("yes", "no")
+students = c(5, 400)
+allstudents = c(5, 600)
+
+#Create random probabilities of categorical answers for data
+random_minority = sample(minority, 463, replace = TRUE, prob = c(0.5, 0.5))
+random_age = rdunif(463, age[2], age[1]) # integers, end first then start 
+random_gender = sample(gender, 463, replace = TRUE, prob = c(0.5, 0.5))
+random_credits = sample(credits, 463, replace = TRUE, prob = c(0.5, 0.5))
+random_beauty = runif(463, beauty[1], beauty[2]) # float, start first then end 
+random_eval = runif(463, eval[1], eval[2])
+random_division = sample(division, 463, replace = TRUE, prob = c(0.5, 0.5))
+random_native = sample(native, 463, replace = TRUE, prob = c(0.5, 0.5))
+random_tenure = sample(tenure, 463, replace = TRUE, prob = c(0.5, 0.5))
+random_students = rdunif(463, students[2], students[1])
+random_allstudents = rdunif(463, allstudents[2], allstudents[1])
+  
+#Compose simulated data in a data frame with all variables.
+simulated_clean <- data.frame(random_minority, random_age, random_gender, random_credits, 
+                              random_beauty, random_eval, random_division, random_native,
+                              random_tenure, random_students, random_allstudents)
+
+#Quick display of table
+tibble(simulated_clean)
+
+
+# compare the end result with the simulation 
+
 
 
 
--- scripts/04-test_data.R
+++ scripts/04-test_data.R
@@ -10,6 +10,41 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(testthat)
 
 #### Test data ####
+
+# check if the number of entries is correct 
+test_that("Data frame has 463 rows", {
+  df <- sim_data 
+  expect_equal(nrow(df), 463)
+})
+
+# making simulated model 
+fm_simulated <- lm(random_eval ~ random_beauty, data = sim_data)
+summary(fm_simulated)
+
+# Test that the p-value is > 0 
+test_that("P-value is > 0 ", {
+  summary <- summary(fm_simulated)
+  expect_true(all(summary$coefficient[, 4] > 0))
+})
+
+#Normally distributed residuals 
+test_that("The residuals are normally distributed", {
+  model <- lm(random_eval ~ random_beauty, data = sim_data)
+  residuals <- resid(model)
+  result <- shapiro.test(residuals)
+  expect_true(result$p.value > 0.05)
+})
+
+# Test that beauty affects eval 
+test_that("The random_beauty variable has a significant effect on the eval", {
+  model <- lm(random_eval ~ random_beauty, data = df)
+  summary <- summary(model)
+  expect_true(summary$coefficients[2,4] < 0.05)
+})
+
+
+
+
